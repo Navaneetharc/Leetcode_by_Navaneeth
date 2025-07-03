af130653c9ed162f8b16648b65203f8c783b57fa
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-07-03 11:03:15 +0530

    Time: 1 ms (66.99%), Space: 55.3 MB (62.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,10 +3,8 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
- let n = nums.length
- let expectedSum = n*(n+1)/2;
- let actualSum = nums.reduce((a,b)=> a+b, 0)
- return expectedSum-actualSum 
-}
-
-    
+let n = nums.length;
+    let expectedSum = (n * (n + 1)) / 2;
+    let actualSum = nums.reduce((acc, val) => acc + val, 0);
+    return expectedSum - actualSum;
+};

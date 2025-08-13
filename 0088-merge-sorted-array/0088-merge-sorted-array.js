00964dd96ec421409e70b0458eb3c96b3ea89f7f
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-08-13 14:57:04 +0530

    Time: 1 ms (12.97%), Space: 54.1 MB (79.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,5 +6,16 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
+    for(let i=0;i<n;i++){
+        nums1[m+i] = nums2[i];
+    }
+    for(let i=1;i<nums1.length;i++){
+        let key = nums1[i];
+        let j = i-1;
+        while(j>=0 && nums1[j]>key){
+            nums1[j+1] = nums1[j];
+            j = j-1;
+        }
+        nums1[j+1] = key;
+    }
 };

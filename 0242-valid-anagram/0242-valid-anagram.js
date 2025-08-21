97d6c8499ac048c47419aa2a99f4f51a2a6d3b1a
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-08-21 10:31:38 +0530

    Time: 12 ms (72.27%), Space: 58.7 MB (36.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,17 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    if(s.length !== t.length) return false;
+    let count = new Map();
+    for(let char of s){
+        count.set(char,(count.get(char) || 0) + 1);
+    }
+    for(let char of t){
+        if(!count.has(char)) return false;
+        count.set(char,count.get(char) - 1);
+        if(count.get(char) === 0){
+            count.delete(char);
+        }
+    }
+    return count.size === 0;
 };

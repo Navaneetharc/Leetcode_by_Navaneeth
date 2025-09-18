3c6a4413a817b4927757fe5479abb9ad87ac5931
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-09-18 15:54:23 +0530

    Time: 0 ms (100%), Space: 54.5 MB (30.51%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.js
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.js
@@ -6,14 +6,15 @@
 var areAlmostEqual = function(s1, s2) {
     if(s1 === s2) return true;
     if(s1.length !== s2.length) return false;
-    
+
     let diff = [];
+
     for(let i=0;i<s1.length;i++){
         if(s1[i] !== s2[i]) diff.push(i);
         if(diff.length > 2) return false;
     }
-    if(diff.length === 0) return true;
 
+    if(diff.length === 0) return true;
     if(diff.length === 2){
         let [i,j] = diff;
         return s1[i] === s2[j] && s1[j] === s2[i];

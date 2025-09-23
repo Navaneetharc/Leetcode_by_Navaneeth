766c0a9440cf561340f24e8efe9907385a857b09
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-09-23 12:23:52 +0530

    Time: 0 ms (100%), Space: 59.3 MB (78.57%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let insert = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] !== 0){
+            nums[insert] = nums[i];
+            insert++; 
+        }
+    }
+
+    while(insert<nums.length){
+        nums[insert] = 0;
+        insert++;
+    }
+};

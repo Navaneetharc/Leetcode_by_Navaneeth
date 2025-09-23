a2961d8d0043a492d3977bb5aa576a22675429a2
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-09-23 11:41:13 +0530

    Time: 2 ms (71.71%), Space: 55.5 MB (89.27%) - LeetHub

--- 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.js
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.js
@@ -12,9 +12,9 @@ var checkPrimeFrequency = function(nums) {
    
     const isPrime = (n) =>{
         if(n<2) return false;
-        if(n === 2) return true;
-        if(n%2 === 0) return false;
-        for(let i=3;i*i<=n;i+=2){
+        // if(n === 2) return true;
+        // if(n%2 === 0) return false;
+        for(let i=2;i*i<=n;i++){
             if(n%i === 0) return false;
         }
         return true;

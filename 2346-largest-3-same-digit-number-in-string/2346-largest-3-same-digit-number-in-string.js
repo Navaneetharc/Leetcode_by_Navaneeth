635f9fe79a2d20ba303b0bdb87afb355a73295a5
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-09-23 17:06:10 +0530

    Time: 10 ms (5.71%), Space: 58.4 MB (5.23%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.js
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.js
@@ -3,14 +3,22 @@
  * @return {string}
  */
 var largestGoodInteger = function(num) {
-    let max = ""
+    let dum = [];
     for(let i=0;i<num.length;i++){
-        if(num[i]==num[i+1] && num[i+1]==num[i+2]){
-            let candidate = num.substring(i,i+3)
-            if(candidate>max){
-                max = candidate
-            }
+        let res = '';
+        if(num[i] == num[i+1] && num[i] == num[i+2]){
+            res += num[i]+num[i+1]+num[i+2];
+            dum.push(res);
         }
     }
-    return max
+    if(dum.length === 0) return ""
+    let large = -Infinity;
+    for(let d of dum){
+        if(d > large){
+            large = d;
+        }
+    }
+    console.log(dum);
+    console.log(large);
+    return large;
 };

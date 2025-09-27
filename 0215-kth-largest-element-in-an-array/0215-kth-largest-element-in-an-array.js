f6ac9e6b77f891169f4d7915639ca3bc236e42c7
Navaneeth CP <cpnavaneeth07@gmail.com>
2025-09-27 10:21:53 +0530

    Time: 62 ms (68.69%), Space: 73.4 MB (14.4%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,6 +4,40 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    nums.sort((a, b) => b - a); 
-    return nums[k - 1];
+    const minHeap = [];
+    
+    function heapifyUp() {
+        let index = minHeap.length - 1;
+        while (index > 0) {
+            let parent = Math.floor((index - 1) / 2);
+            if (minHeap[parent] > minHeap[index]) {
+                [minHeap[parent], minHeap[index]] = [minHeap[index], minHeap[parent]];
+                index = parent;
+            } else break;
+        }
+    }
+    
+    function heapifyDown() {
+        let index = 0, length = minHeap.length;
+        while (true) {
+            let left = 2 * index + 1, right = 2 * index + 2, smallest = index;
+            if (left < length && minHeap[left] < minHeap[smallest]) smallest = left;
+            if (right < length && minHeap[right] < minHeap[smallest]) smallest = right;
+            if (smallest !== index) {
+                [minHeap[index], minHeap[smallest]] = [minHeap[smallest], minHeap[index]];
+                index = smallest;
+            } else break;
+        }
+    }
+    
+    for (let num of nums) {
+        minHeap.push(num);
+        heapifyUp();
+        if (minHeap.length > k) {
+            minHeap[0] = minHeap.pop();
+            heapifyDown();
+        }
+    }
+    
+    return minHeap[0];
 };

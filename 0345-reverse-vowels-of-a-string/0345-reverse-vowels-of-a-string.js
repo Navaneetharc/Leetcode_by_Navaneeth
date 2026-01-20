4613927336052b50d4f1370df6d7f065c5cffb01
Navaneeth CP <cpnavaneeth07@gmail.com>
2026-01-20 11:54:43 +0530

    Time: 7 ms (66.13%), Space: 60.2 MB (63.09%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -3,18 +3,13 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
-    let vows = 'AEIOUaeiou';
-    let queue = [];
-    for(let i=0;i<s.length;i++){
-        if(vows.includes(s[i])){
-           queue.push(s[i]);
-        }       
-    }
-    let t = s.split('');
-    for(let j=0;j<t.length;j++){
-        if(vows.includes(t[j])){
-            t[j] = queue.pop();
+    let vows = "AEIOUaeiou".split('');
+    let word = s.split('')
+    let incVows = s.split('').filter(items => vows.includes(items));
+    for(let i=0;i<word.length;i++){
+        if(vows.includes(word[i])){
+            word[i] = incVows.pop();
         }
     }
-    return t.join('');
+    return word.join('');
 };

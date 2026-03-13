2af88b1b07e1915c67675edd805954e0dcbee8cc
Navaneeth CP <cpnavaneeth07@gmail.com>
2026-03-13 17:07:02 +0530

    Time: 1 ms (89.18%), Space: 56 MB (64.07%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
@@ -3,29 +3,12 @@
  * @return {number[]}
  */
 var getSneakyNumbers = function(nums) {
+
     let arr = [];
     for(let i=0;i<nums.length;i++){
-        let count = 0;
-        for(let j=0;j<nums.length;j++){
-            if(nums[i] === nums[j] && i!==j){
-                count++;
-            }
-        }
-        console.log(count)
-        if(count > 0){
-            arr.push(nums[i]);
+        if(nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])){
+            if(!arr.includes(nums[i])) arr.push(nums[i]);
         }
     }
-
-    let arr2 = [];
-    let arr3 = []
-    
-    for(let i=0;i<arr.length;i++){
-        if(!arr2.includes(arr[i])){
-            arr2.push(arr[i]);
-            arr3.push(arr[i]);
-        }
-    }
-
-    return arr3
+    return arr;
 };
